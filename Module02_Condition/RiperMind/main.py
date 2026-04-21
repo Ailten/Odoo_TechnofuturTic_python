6380104e1da74bd739a78e81f0fa5d79a207e0a7
Faouzi Benabbas <f.benabbas@stag.technofuturtic.education>
2026-04-21 10:24:28 +0200

    advence on riper mind

--- Module02_Condition/RiperMind/main.py
+++ Module02_Condition/RiperMind/main.py
@@ -7,24 +7,16 @@ size_map_y = 10
 
 class RiperMind():
     bombs = []
+    cels = []
+    isLose = False
 
 # print screen.
 def printScreen():
-    for y in range(0, size_map_y, 1):
+    for y in range(len(RiperMind.cels)):
         str_line = ''
-        for x in range(0, size_map_x, 1):
-            bomb = (x, y)
-            cel = '.'
+        for x in range(len(RiperMind.cels[y])):
+            str_line += RiperMind.cels[y][x]['char'] if RiperMind.cels[y][x]['isFind'] else '#'
 
-            if findInBombs(bomb):
-                cel = 'O'
-            else:
-                count_bomb_adj = countBombsAdj(bomb)
-                if count_bomb_adj > 0:
-                    cel = str(count_bomb_adj)
-
-            str_line += cel
-        print(str_line)
 
 # generate bomb.
 def generateBomb():
@@ -64,6 +56,80 @@ def countBombsAdj(pos) -> int:
     output += 1 if findInBombs((x  , y+1)) else 0
     return output
 
+# generateCels.
+def generateCels():
+    RiperMind.cels = []
+    for y in range(0, size_map_y, 1):
+        RiperMind.cels.append([])
+        for x in range(0, size_map_x, 1):
+            bomb = (x, y)
+            cel = '.'
+
+            if findInBombs(bomb):
+                cel = 'O'
+            else:
+                count_bomb_adj = countBombsAdj(bomb)
+                if count_bomb_adj > 0:
+                    cel = str(count_bomb_adj)
+
+            RiperMind.cels[y].append({
+                'char': cel,
+                'isFind': False
+            })
+
+# try a pos.
+def tryAPos(pos_try):
+    if findInBombs(pos_try):
+        RiperMind.isLose = True
+        return
+    revealACel(pos_try)
+def revealACel(pos):
+    if pos[0] < 0 or pos[0] >= size_map_x:
+        return
+    if pos[1] < 0 or pos[1] >= size_map_y:
+        return
+    cel = RiperMind.cels[pos[1]][pos[0]]
+    if cel['isFind']:
+        return
+    RiperMind.cels[pos[1]][pos[0]]['isFInd'] = True
+    if cel['char'] == '.':
+        revealACel((pos[0]+1, pos[1]+1))  # FIXME: recursiv call to hight.
+        revealACel((pos[0]+1, pos[1]  ))
+        revealACel((pos[0]+1, pos[1]-1))
+        revealACel((pos[0]-1, pos[1]+1))
+        revealACel((pos[0]-1, pos[1]  ))
+        revealACel((pos[0]-1, pos[1]-1))
+        revealACel((pos[0]  , pos[1]+1))
+        revealACel((pos[0]  , pos[1]-1))
+    
+
+
 # execute.
 generateBomb()
+generateCels()
 printScreen()
+
+while True:
+
+    input_x = None
+    input_y = None
+
+    try:
+        input_x = int(input("enter X pos to try.\n"))
+        input_y = int(input("enter Y pos to try.\n"))
+    except:
+        print("an input enter is not a number !")  # verify number.
+        continue
+
+    # verify range.
+    if input_x < 0 or input_x >= size_map_x:
+        print("input X is out of range !")
+        continue
+    if input_y < 0 or input_y >= size_map_x:
+        print("input Y is out of range !")
+        continue
+
+    pos_try = (input_x, input_y)
+    tryAPos(pos_try)
+
+    printScreen()
