0998a2e0259c9290e2549a393e78101cd8994ada
Ailten <faouzi26b@live.fr>
2026-04-22 10:32:12 +0200

    directory - package

--- Module04_Function/Exo14.py
+++ Module04_Function/Exo14.py
@@ -1,6 +1,18 @@
 
-def my_func(*n: str) -> int:
-    return len(n)
+# read a file txt with yield.
 
-a = my_func
-print(a('a', 'b'))
+def readFile(path_file):
+    with open(path_file, 'r') as file:
+        for line in file:
+            yield line.strip()
+
+path_txt = 'Module04_Function\\text.txt'
+list_line = readFile(path_txt)
+print('\n'.join(list_line))
+
+word_count = sum([1 for _ in list_line])
+print(word_count)
+
+
+from utils.file_utils import my_function, my_function_2  # import a function from a file (in a package).
+#from utils.file_utils import *  # import all.
